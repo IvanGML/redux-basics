6a49a85fe4ceed1a95f8c706ff321a667d1d6891
IvanGML <somovlife@gmail.com>
2017-08-09 10:01:11 +0300

    handmade redux

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from './redux';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return {count: state.count + action.amount};
+        case "DECREMENT":
+            return {count: state.count - action.amount};
+        case "RESET":
+            return {count: 0};
+        default:
+            return state;
+    }
+}
+
+const incrementAction = {type: 'INCREMENT', amount: 1};
+const decrementAction = {type: 'DECREMENT', amount: 1};
+const resetAction = {type: 'RESET'};
+
+const store = createStore(reducer, initialState);
 
 class Counter extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { count: 0 };
-
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    componentDidMount() {
+        store.subscribe(()=>this.forceUpdate());
     }
 
     increment() {
-        this.setState({ count: this.state.count + 1 });
+        store.dispatch(incrementAction);
     }
 
     decrement() {
-        this.setState({ count: this.state.count - 1 });
+        store.dispatch(decrementAction);
+    }
+
+    reset() {
+        store.dispatch(resetAction);
     }
 
     render() {
+        const count = store.getState().count;
         return (
             <div className="counter">
-                <span className="count">{this.state.count}</span>
+                <span className="count">{count}</span>
 
                 <div className="buttons">
                     <button className="decrement" onClick={this.decrement}>-</button>
+                    <button className="reset" onClick={this.reset}>0</button>
                     <button className="increment" onClick={this.increment}>+</button>
                 </div>
             </div>
