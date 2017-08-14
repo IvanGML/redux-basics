9eeebfe282c0666e81adc6c635ca72e916e5c5d3
IvanGML <somovlife@gmail.com>
2017-08-14 16:23:35 +0300

    filter implementation. start

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import reducer from './reducers';
 import state from './state';
 import App from './App';
 
-const store = createStore(reducer, state);
+const store = createStore(reducer, state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
