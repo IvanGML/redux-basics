a972de298258901c4cba4b9b6c59e4920601bb94
IvanGML <somovlife@gmail.com>
2017-08-23 15:20:08 +0300

    Devtools have been added.

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,6 @@ import { createStore } from 'redux';
 import reducer from '../reducers';
 import state from './state';
 
-const store = createStore(reducer, state);
+const store = createStore(reducer, state, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
